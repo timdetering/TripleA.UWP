12909d18696e81daaaed709f8080bf1cd737198a
Mark Tinderholt <markti@outlook.com>
2016-12-18 23:47:04 -0500

    Mvvm light, uwp toolkit hamburger and blade view

---
+++ TripleA/ViewModel/MenuItemViewModel.cs
@@ -0,0 +1,29 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripleA.ViewModel
+{
+    public class MenuItemViewModel : ViewModelBase
+    {
+        public string Label { get; set; }
+
+        private char symbolAsChar;
+        public char SymbolAsChar
+        {
+            get
+            {
+                return symbolAsChar;
+            }
+            set
+            {
+                symbolAsChar = value;
+                RaisePropertyChanged();
+            }
+        }
+        public Type PageType { get; set; }
+    }
+}
