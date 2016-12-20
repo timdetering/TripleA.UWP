698db82105be0bb6b91bdcf6726ee9afa9e8faa4
Mark Tinderholt <markti@outlook.com>
2016-12-19 22:55:50 -0500

    page title change event

--- TripleA/ViewModel/MainViewModel.cs
+++ TripleA/ViewModel/MainViewModel.cs
@@ -1,30 +1,60 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Messaging;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TripleA.Events;
 using TripleA.Views;
 
 namespace TripleA.ViewModel
 {
-    public class MainViewModel
+    public class MainViewModel : ViewModelBase
     {
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set { title = value; RaisePropertyChanged(); }
+        }
+
         public ObservableCollection<MenuItemViewModel> MenuItems { get; private set; }
+        public ObservableCollection<MenuItemViewModel> Options { get; private set; }
 
         public MainViewModel()
         {
             this.MenuItems = new ObservableCollection<MenuItemViewModel>();
+            this.Options = new ObservableCollection<MenuItemViewModel>();
 
-            this.InitializeOptions();
+            this.InitializeMenu();
+            InitializeEventHandlers();
         }
 
-        private void InitializeOptions()
+        private void InitializeEventHandlers()
+        {
+            Messenger.Default.Register<PageTitleChangedEvent>(this, HandlePageTitleChangedEvent);
+        }
+
+        private void HandlePageTitleChangedEvent(PageTitleChangedEvent payload)
+        {
+            this.Title = payload.Title;
+        }
+
+        private void InitializeMenu()
         {
             this.MenuItems.Clear();
             var mapListItem = new MenuItemViewModel() { Label = "Map", SymbolAsChar = '\uE707', PageType = typeof(MapView) };
 
             this.MenuItems.Add(mapListItem);
+        }
+        private void InitializeOptions()
+        {
+            this.Options.Clear();
+            var mapListItem = new MenuItemViewModel() { Label = "New Game", SymbolAsChar = '\uE707', PageType = typeof(NewGameView) };
+
+            this.Options.Add(mapListItem);
         }
     }
 }
